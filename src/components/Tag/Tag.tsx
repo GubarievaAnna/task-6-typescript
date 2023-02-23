ecdcfb90980445d0ccf7f814dc72488885e5000a
GubarievaAnna <annagubarieva@gmail.com>
2023-02-23 12:48:50 +0200

    Update Tag.tsx

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -11,3 +11,29 @@ const Tag: React.FC<ITag> = ({ label, type }) => {
 };
 
 export default Tag;
+
+
+// import React from 'react';
+// import s from "./Card.scss";
+
+
+// interface CardProps {
+//   size: string,
+//   number: number,
+//   time: number,
+
+// }
+
+// const Card: React.FC<CardProps> = ({
+//   size = "small",
+//   number = 50,
+//   time = 2
+// }) => {
+//   return (
+//     <li className={`${s[`card--${size}`]}`}>
+
+//     </li>
+//   )
+// }
+
+// export default Card;
